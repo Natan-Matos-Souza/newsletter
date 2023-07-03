5d64076b6c5b8698aa42e74b4c81d016f308157f
natan-matos-souza <natanmatos_18@hotmail.com>
2023-07-03 20:17:14 -0300

    Finalizando página index

--- index.php
+++ index.php
@@ -38,72 +38,83 @@ if (isSet($_POST['username']) && isSet($_POST['email'])) {
     } else {
 
         print "Email inválido!";
+        return;
 
     }
 
 
     require "database_connection.php";
     
-    $userHash = rand(1,100000);
 
-    $userHash = password_hash($userHash, PASSWORD_BCRYPT);
+    //Verifica se já há algum usuário cadastrado utilizando as mesmas informações.
+    $databaseSearch = $databaseConnection->query("SELECT useremail FROM assinantes WHERE useremail='$userEmail'");
+    $searchResult = $databaseSearch->num_rows;
 
-
-    $databaseConnection->query("INSERT INTO assinantes VALUES ('$userHash', '$userName', '$userEmail')");
-
-
-
-
-    require "vendor/autoload.php";
-
-    //Cria uma nova instância da classe PHPMailer.
-    $mail = new PHPMailer(true);
-    //Coloca os caracteres UFT-8.
-    $mail->CharSet = 'UTF-8';
-    $mail->Encoding = "base64";
-
-    try {
-        $mail->isSMTP(); //Configura o e-mail para SMTP.
-        $mail->Host = 'smtp.gmail.com'; //Host do provedor de e-mail.
-        $mail->SMTPAuth = true;
-        $mail->Username = $_ENV['hostEmail']; //Informações da conta e-mail.
-        $mail->Password = $_ENV['emailPass']; //Informações da senha e-mail.
-        $mail->SMTPSecure= 'ssl'; //Utilizando SSL.
-        $mail->Port = 465;
-
-        $mail->setFrom($_ENV['hostEmail']); //Remetente.
-        $mail->addAddress($userEmail); //Destinatário.
-
-        $mail->isHTML(true); //Informa se o e-mail contém HTML.
-        $mail->Subject = "Seja bem-vindo, $userName"; //Título do e-mail.
-
-        $mail->Body = <<<_HTML_
-        <h1>Olá, $userName!</h1>
-        <br>
-        <br>
-        <h2>Você foi cadastrado em nossa Newsletter com sucesso!</h2>
-        <br>
-        <br>
-        <h2>Atenciosamente,<br>Natan Matos!</h2>
-        <br>
-        <br>
-        <p>Caso você queira cancelar a Newsletter, <a href="http://localhost/test/newslettercancel.php?hash=$userHash">clique aqui</a>.</p>
-        _HTML_;
-        $mail->AltBody = "Olá, Natan Matos! Estou testando o PHPMailer!"; //Mensagem alternativa caso não haja suporte ao HTML.
-
-        $mail->send(); //Envia a mensagem.
-
-        $emailSended = true;
-
-
-
-    } catch (Exception $e) {
-        print "O e-mail não foi enviado. Motivo: $e";
+    if ($searchResult > 0) {
+        $emailSended = array(false, 'email-already-registered');
     }
+
+
+    if ($searchResult < 1) {
+
+        $userHash = rand(1,100000);
+
+        $userHash = password_hash($userHash, PASSWORD_BCRYPT);
+
+        $databaseConnection->query("INSERT INTO assinantes VALUES ('$userHash', '$userName', '$userEmail')");
+
+
+        require "vendor/autoload.php";
+
+        //Cria uma nova instância da classe PHPMailer.
+        $mail = new PHPMailer(true);
+        //Coloca os caracteres UFT-8.
+        $mail->CharSet = 'UTF-8';
+        $mail->Encoding = "base64";
+
+        try {
+            $mail->isSMTP(); //Configura o e-mail para SMTP.
+            $mail->Host = 'smtp.gmail.com'; //Host do provedor de e-mail.
+            $mail->SMTPAuth = true;
+            $mail->Username = $_ENV['hostEmail']; //Informações da conta e-mail.
+            $mail->Password = $_ENV['emailPass']; //Informações da senha e-mail.
+            $mail->SMTPSecure= 'ssl'; //Utilizando SSL.
+            $mail->Port = 465;
+
+            $mail->setFrom($_ENV['hostEmail']); //Remetente.
+            $mail->addAddress($userEmail); //Destinatário.
+
+            $mail->isHTML(true); //Informa se o e-mail contém HTML.
+            $mail->Subject = "Seja bem-vindo, $userName"; //Título do e-mail.
+
+            $mail->Body = <<<_HTML_
+            <h1>Olá, $userName!</h1>
+            <br>
+            <br>
+            <h2>Você foi cadastrado em nossa Newsletter com sucesso!</h2>
+            <br>
+            <br>
+            <h2>Atenciosamente,<br>Natan Matos!</h2>
+            <br>
+            <br>
+            <p>Caso você queira cancelar a Newsletter, <a href="http://localhost/test/newslettercancel.php?hash=$userHash">clique aqui</a>.</p>
+            _HTML_;
+            $mail->AltBody = "Olá, Natan Matos! Estou testando o PHPMailer!"; //Mensagem alternativa caso não haja suporte ao HTML.
+
+            $mail->send(); //Envia a mensagem.
+
+            $emailSended = array(true, 'sucessful-sended');
+
+
+
+        } catch (Exception $e) {
+            print "O e-mail não foi enviado. Motivo: $e";
+            $emailSended = array(false, 'internal-problem');
+        }
+    }
+
+
 }
-
-
-
 
 
 ?>
@@ -165,20 +176,36 @@ if (isSet($_POST['username']) && isSet($_POST['email'])) {
 
 
     </main>
+            
 
 
+            <?php
+                //#C4D7B2 -> cor verde;
+                if ($emailSended[0] == false && $emailSended[1] == 'email-already-registered') {
 
-            <?php if ($emailSended == true) {
+                    $containerMessage = 'Erro: este e-mail já foi cadastrado!';
+                    $containerColor = '#FF0000';
+                }
+
+                if ($emailSended[0] == false && $emailSended[1] == 'internal-problem') {
+
+                    $containerMessage = 'Erro: não foi possível cadastrar o seu e-mail';
+                    $containerColor = 'FF0000';
+                }
+
+                if ($emailSended[0] == true) {
+                    $containerMessage = 'E-mail cadastrado com sucesso!';
+                    $containerColor = '#C4D7B2';
+                }
 
                 print <<<_HTML_
 
                 <div class="email-status-container">
-                    <h2>O email foi enviado com sucesso!</h2>
+                    <h2>$containerMessage</h2>
                 </div>
 
                 <style>
                     .email-status-container {
-                        display: flex;
                         animation: email-status-animation 4s;
                         position: fixed;
                         font-family: 'Roboto', sans-serif;
@@ -187,14 +214,14 @@ if (isSet($_POST['username']) && isSet($_POST['email'])) {
                         top: 40px;
                         right: -100%;
                         padding: 30px;
-                        background-color: #C4D7B2;
+                        background-color: $containerColor;
                         border-radius: 4px;
                     }
                 </style>
 
                 _HTML_;
 
-            }
+            
 
             ?>
         
