de54dcddb2066f03970e9316b2c217837a22fd48
natan-matos-souza <natanmatos_18@hotmail.com>
2023-07-03 23:47:14 -0300

    Adicionando aúdio

--- js/index.js
+++ js/index.js
@@ -1,73 +1,80 @@
-const changeInputBtn = document.getElementById('change-input-btn')
-const submitBtn = document.getElementById('submit-btn')
+//Todo o app está dentro de uma função como medida para evitar colisão de variáveis.
+
+function app() {
+    const changeInputBtn = document.getElementById('change-input-btn')
+    const submitBtn = document.getElementById('submit-btn')
 
 
-submitBtn.addEventListener('click', (event) => {
-    
-    if (secondInputDiv.style.display === "block") {
-        event.submit()
-    } else {
-        event.preventDefault()
-    }
-
-})
-
-
-//Importando inputs//
-const firstInput = document.getElementById('first-input')
-const secondInput = document.getElementById('second-input')
-
-//Importando divs
-
-const firstInputDiv = document.getElementById('first-input-div')
-const secondInputDiv = document.getElementById('second-input-div')
-
-const formText = document.getElementById('text')
-
-firstInput.focus()
-
-
-
-changeInputBtn.addEventListener('click', () => {
-
-    formText.style.animation = 'changeFormText 3s';
-
-    setTimeout(() => {
-        formText.innerHTML = 'Digite o seu e-mail:'
-    }, 2 * 1000)
-
-    firstInputDiv.style.display = "none"
-    secondInputDiv.style.display = "block"
-    changeInputBtn.style.display = "none"
-    submitBtn.style.display = "inline-block";
-    secondInput.focus()
-
-})
-
-//Error log
-
-const logText = document.querySelector('#log-status').innerHTML;
-
-console.log(logText)
-
-document.querySelector('body').addEventListener('click', async () => {
-
-    let count = 0
-    const errorAudio = document.querySelector('audio')
-
-
-    if (logText == "Erro: este e-mail já foi cadastrado!" || logText == "Erro: não foi possível cadastrar o seu e-mail") {
-
-        if (count < 1) {
-
-            console.log('Código executado!');
-
-            errorAudio.play();
-
-            count++;
-
+    submitBtn.addEventListener('click', (event) => {
+        
+        if (secondInputDiv.style.display === "block") {
+            event.submit()
+        } else {
+            event.preventDefault()
         }
 
-    }
-})
+    })
 
+
+    //Importando inputs//
+    const firstInput = document.getElementById('first-input')
+    const secondInput = document.getElementById('second-input')
+
+    //Importando divs
+
+    const firstInputDiv = document.getElementById('first-input-div')
+    const secondInputDiv = document.getElementById('second-input-div')
+
+    const formText = document.getElementById('text')
+
+    firstInput.focus()
+
+
+
+    changeInputBtn.addEventListener('click', () => {
+
+        formText.style.animation = 'changeFormText 3s';
+
+        setTimeout(() => {
+            formText.innerHTML = 'Digite o seu e-mail:'
+        }, 2 * 1000)
+
+        firstInputDiv.style.display = "none"
+        secondInputDiv.style.display = "block"
+        changeInputBtn.style.display = "none"
+        submitBtn.style.display = "inline-block";
+        secondInput.focus()
+
+    })
+
+    //Error log
+
+    const logText = document.querySelector('#log-status').innerHTML;
+
+    console.log(logText)
+
+    let count = 0
+
+    function playErrorSound() {
+
+        setTimeout(() => {
+
+            if (count < 1) {
+
+                const errorSound = document.querySelector('audio');
+        
+                errorSound.play();
+
+                count++
+            }
+    
+        }, 1 * 1000)
+    }
+
+    if (logText == "Erro: este e-mail já foi cadastrado!" || logText == "Erro: não foi possível cadastrar o seu e-mail") {
+        playErrorSound();
+    }
+
+}
+
+app()
