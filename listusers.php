696b56df47b1c80f39ad9df7be94295b7fe5249e
natan-matos-souza <natanmatos_18@hotmail.com>
2023-07-09 14:50:09 -0300

    Criando listusers

--- listusers.php
+++ listusers.php
@@ -1,35 +1,39 @@
-<?php
+<?php 
 
 session_start();
 
-if (empty($_SESSION['admin']) || $_SESSION['admin'] != true) {
+if (!isSet($_SESSION['admin']) || $_SESSION['admin'] != true) {
+
     Header("Location: http://localhost/test/login");
     return;
+
 }
 
 require "database_connection.php";
 
 if (isSet($_GET['username'])) {
 
-    $userName = $_GET['username'];
+    $userName = filter_input(INPUT_GET, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
 
-    $dbSearch = $databaseConnection->query("SELECT * FROM assinantes WHERE username LIKE '$userName%'");
+    $dbSearch = $databaseConnection->query("SELECT username, useremail FROM assinantes WHERE username LIKE '$userName%'");
 
     $rowCount = $dbSearch->num_rows;
 
     if ($rowCount < 1) {
-        print 'Nenhum usuário encontrado!';
+
+        http_response_code(404);
+        print json_encode("Não há usuários com esse nome");
         return;
+
     }
 
-}
+    $usersInfo = $dbSearch->fetch_all(MYSQLI_ASSOC);
 
-if (empty($_GET['username'])) {
+    print json_encode($usersInfo);
+    http_response_code(200);
+    return;
 
-    $dbSearch = $databaseConnection->query("SELECT * FROM assinantes");
-
-    $rowCount = $dbSearch->num_rows;
-
+    
 }
 
 ?>
@@ -39,41 +43,27 @@ if (empty($_GET['username'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Página para listar usuários</h1>
 
-    <?php
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 
-    if (isSet($userName)) {
-        for ($i = 0; $i < $rowCount; $i++) {
+<script>
 
-            $userInfo = $dbSearch->fetch_array();
+    let usersInfo;
 
-            print <<<_HTML_
-            $userInfo[1]<br>
-            $userInfo[2]<br>
-            <hr>
-            _HTML_;
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: 'http://localhost/test/listusers',
+        data: {
+            username: 'Natan'
         }
-    }
+    }).done(function(result) {
+        usersInfo = result;
+    })
+</script>
 
-    if (empty($_GET['username'])) {
-
-        for ($i = 0; $i < $rowCount; $i++) {
-
-            $userInfo = $dbSearch->fetch_array();
-
-            print <<<_HTML_
-            $userInfo[1]<br>
-            $userInfo[2]<br>
-            <hr>
-            _HTML_;
-        }
-
-    }
-
-    ?>
 </body>
 </html>
