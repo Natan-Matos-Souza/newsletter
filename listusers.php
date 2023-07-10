a2c8ab40434a13d577dd94a5cae086bec9a3f67f
natan-matos-souza <natanmatos_18@hotmail.com>
2023-07-10 13:28:05 -0300

    Criando listusers

--- listusers.php
+++ listusers.php
@@ -1,12 +1,10 @@
-<?php 
+<?php
 
 session_start();
 
 if (!isSet($_SESSION['admin']) || $_SESSION['admin'] != true) {
-
     Header("Location: http://localhost/test/login");
     return;
-
 }
 
 require "database_connection.php";
@@ -15,58 +13,57 @@ if (isSet($_GET['username'])) {
 
     $userName = filter_input(INPUT_GET, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
 
-    //Realiza uma consulta no banco de dados
     $dbSearch = $databaseConnection->query("SELECT username, useremail FROM assinantes WHERE username LIKE '$userName%'");
 
     $rowCount = $dbSearch->num_rows;
 
     if ($rowCount < 1) {
-
-        //Retorna status code 404 caso não encontre o usuário
+        print json_encode("Não existe usuários com nome $userName", JSON_UNESCAPED_UNICODE);
         http_response_code(404);
-        print json_encode("Não há usuários com esse nome");
         return;
-
     }
 
-    //Retorna um JSON com os dados encontrados no banco de dados.
     $usersInfo = $dbSearch->fetch_all(MYSQLI_ASSOC);
+
+    $rowCount = [
+            "members" => $rowCount
+    ];
+
+    array_push($usersInfo, $rowCount);
 
     print json_encode($usersInfo);
     http_response_code(200);
     return;
-
-    
 }
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Natan Matos</title>
+        <meta name="description" content="Página para listar usuários">
+    </head>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <body>
 
-<script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
 
-    let usersInfo;
+    <script>
+            let usersInfo;
 
-    $.ajax({
-        type: 'GET',
-        dataType: 'json',
-        url: 'http://localhost/test/listusers',
-        data: {
-            username: 'Natan'
-        }
-    }).done(function(result) {
-        usersInfo = result;
-    })
-</script>
-
-</body>
+            $.ajax({
+                type: 'GET',
+                dataType: 'json',
+                url: 'http://localhost/test/listusers',
+                data: {
+                    username: 'Natan'
+                }
+            }).done(function(result) {
+                usersInfo = result;
+            }).fail(function(result) {
+                console.log(result.responseJSON);
+            })
+        </script>
+    </body>
 </html>
