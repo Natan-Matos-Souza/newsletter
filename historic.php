7dd23e244bd795cb535468babe774e8767fac6c8
natan-matos-souza <natanmatos_18@hotmail.com>
2023-07-11 16:13:38 -0300

    Listando e-mails

--- historic.php
+++ historic.php
@@ -10,9 +10,9 @@ require "database_connection.php";
 
 global $databaseConnection;
 
-if (isSet($_GET['get_historic']) && !empty($_GET['get_historic'])) {
+if (isSet($_GET['get_historic']) && empty($_GET['get_historic'])) {
 
-    $dbSearch = $databaseConnection->query("SELECT * FROM emailhistory");
+    $dbSearch = $databaseConnection->query("SELECT * FROM emailhistory ORDER BY id DESC");
 
     $rowCount = $dbSearch->num_rows;
 
@@ -30,6 +30,27 @@ if (isSet($_GET['get_historic']) && !empty($_GET['get_historic'])) {
 
 }
 
+if (isSet($_GET['get_historic']) && !empty($_GET['get_historic'])) {
+
+    $emailName = filter_input(INPUT_GET, 'get_historic', FILTER_SANITIZE_SPECIAL_CHARS);
+
+    $dbSearch = $databaseConnection->query("SELECT * FROM emailhistory WHERE email_title, email_content='$emailName'");
+
+    $rowCount = $dbSearch->num_rows;
+
+    if ($rowCount < 1) {
+        print json_encode("Não exite e-mails com esse nome", JSON_UNESCAPED_UNICODE);
+        http_response_code(404);
+        return;
+    }
+
+    $emailFound = $dbSearch->fetch_all(MYSQLI_ASSOC);
+    http_response_code(200);
+    return;
+
+
+}
+
 
 ?>
 
@@ -37,11 +58,25 @@ if (isSet($_GET['get_historic']) && !empty($_GET['get_historic'])) {
     <head>
         <meta charset="UTF-8">
         <meta name="description" content="Histórico de e-mails">
-        <tite>Histórico de e-mails</tite>
+        <link rel="stylesheet" href="style/historic.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,500;0,700;1,100;1,300;1,400;1,500&display=swap" rel="stylesheet">
+        <title>Histórico de e-mails</title>
     </head>
 
     <body>
         <main>
+
+            <div class="logo-area">
+                <img src="assets/logo-dark-mode.png" alt="Logo" class="logo-img">
+            </div>
+
+            <h2 class="historic-title">Aguarde...</h2>
+
+            <div class="email-container">
+
+            </div>
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
             <script src="js/historic.js "></script>
         </main>
--- js/historic.js
+++ js/historic.js
@@ -1,5 +1,37 @@
 let emailHistoric;
 
+
+function showData(result)
+{
+    const emailContainer = document.querySelector('.email-container');
+    const emailAreaResponseText = document.querySelector('.historic-title');
+
+    emailAreaResponseText.innerHTML = 'Histórico';
+
+    result.forEach((element) => {
+
+        if (element.sent == 1) {
+            element.sent = 'Enviado';
+        } else {
+            element.sent = 'Enviando...';
+        }
+
+        emailContainer.innerHTML += `<div class="email-card"><div class="title-area"><span class="email-title">${element.email_title}</span></div><div class="content-area"><span class="email-content">${element.email_content}</span></div><div class="email-status-area"><span class="email-status">${element.sent}</span></div></div>`;
+
+    });
+
+    const statusText = document.getElementsByClassName('email-status');
+
+    const newArray = [...statusText];
+
+    newArray.forEach((element) => {
+        if (element.innerHTML == 'Enviado') {
+            element.style.color = '#C4D7B2';
+        } else {
+            element.style.color = '#FF0000';
+        }
+    })
+}
 function getEmails()
 {
     $.ajax({
@@ -7,10 +39,10 @@ function getEmails()
         dataType: 'json',
         url: 'http://localhost/test/historic',
         data: {
-            get_historic: 'a'
+            get_historic: ''
         }
-    }).done(function(reuslt) {
-        emailHistoric = reuslt;
+    }).done(function(result) {
+        showData(result);
     }).fail(function (result) {
         console.log(result.responseJSON);
     })
